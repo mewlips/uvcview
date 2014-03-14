0e49ee3d725e4942488f3b2ffab2b687248ae606
Mewlips <mewlips@gmail.com>
2014-03-14 18:32:20 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,13 @@
 mkdir -p bin lib
 
 gcc -shared -fpic -o lib/libxioctl.so src/uvcview/xioctl.c
-rustc -L lib -L ../../repos/rust-sdl/lib src/uvcview/main.rs -o bin/uvcview
+
+BS=
+old_ifs="$IFS"
+IFS=":"
+for pkg_path in $RUST_PATH; do
+    LIBS="$LIBS -L $pkg_path/lib"
+done
+IFS="$old_ifs"
+
+rustc -L lib $LIBS  src/uvcview/main.rs -o bin/uvcview
